84da21d3199bbc886cb004042568ce48a74eb7ce
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-05-07 22:18:38 +0500

    pages removed

--- pages/masterpiece.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Materpiece = () => {
-  return (
-    <div>Materpiece</div>
-  )
-}
--- pages/rankings.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Rankings = () => {
-  return (
-    <div>Rankings</div>
-  )
-}

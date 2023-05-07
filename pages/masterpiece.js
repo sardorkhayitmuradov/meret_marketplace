c9da231415ec81b0a2719f630c1057ee81569e7e
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-05-07 21:58:27 +0500

    pages added

---
+++ pages/masterpiece.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Materpiece = () => {
+  return (
+    <div>Materpiece</div>
+  )
+}
---
+++ pages/rankings.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Rankings = () => {
+  return (
+    <div>Rankings</div>
+  )
+}

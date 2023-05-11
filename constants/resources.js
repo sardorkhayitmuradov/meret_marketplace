dd7f33bf06cd672171bbc3f3d9921fac1eb1cecc
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2023-05-11 17:20:31 +0500

    home page finished

---
+++ constants/resources.js
@@ -0,0 +1,34 @@
+export const resources = [
+    {
+        "id": 1,
+        "image": "/images/resources1.svg",
+        "name": "Freak In Me",
+        "author": "Tommy lee",
+        "verified": true,
+        "ethereum": "24,328.16",
+        "changes": "+219.48%",
+        "category": "Art",
+    },
+    {
+        "id": 2,
+        "image": "/images/resources2.svg",
+        "category": "Art",
+        "name": "Wonderful Tonight",
+        "author": "Tommy lee",
+        "verified": false,
+        "ethereum": "24,328.16",
+        "changes": "+219.48%",
+    },
+    {
+        "id": 3,
+        "image": "/images/resources3.svg",
+        "category": "Art",
+        "author": "Tommy lee",
+        "verified": false,
+        "name": "Mine",
+        "author": "Tommy lee",
+        "verified": false,
+        "ethereum": "24,328.16",
+        "changes": "+219.48%",
+    }
+]
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { Button } from "@/components/button";
 import TimeSwitcher from "@/components/switchers/daySwitcher/index.js";
 
+import { topCollections } from "@/constants/topCollections.js";
+import { resources } from "@/constants/resources.js";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from 'swiper';
 import { collections } from '../constants/collections.js'
@@ -16,6 +19,9 @@ import ArrowRight from '../public/images/rightSmall.svg'
 import Video from '../public/images/videoStarter.svg'
 import Right from '../public/images/exploreRight.svg'
 import Left from '../public/images/exploreLeft.svg'
+import ethereum from '../public/images/ethereum.svg'
+import heroVideo from '../public/images/videoBanner.svg'
+import VideoStarter from '../public/images/videoStarter.svg'
 
 const options = [
   { value: '7', label: 'last 7 days' },
@@ -224,9 +230,81 @@ export default function Home() {
       </section>
       <section className="py-12">
         <div className="max-w-xl mx-auto w-full">
-          <div className="flex items-center justify-end">
+          <div className="flex items-center justify-end mb-12">
             <h2 className="font-NunitoSansBold text-xl-bold relative -left-[30%] text-slate-900">Top collections over</h2>
             <TimeSwitcher options={options} onChange={handleOptionChange} />
+          </div>
+          <ul className="flex items-center flex-wrap gap-6 justify-between">
+            {
+              topCollections.map((tc) => {
+                return (
+                  <li key={tc.id} className="flex items-center gap-6 max-w-[563px] w-full">
+                    <p className="text-xs-regular font-NunitoSansBold text-slate-900">{tc.id}</p>
+                    <div className="flex items-center gap-6 w-full">
+                      <Image src={tc.image} width={48} height={48} alt='tc1' />
+                      <div className="w-full">
+                        <h4 className="text-xs-regular font-NunitoSansBold text-slate-900">{tc.name}</h4>
+                        <p className="text-slate-300 font-NunitoSansSemiBold text-xs-bold">{tc.author}</p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-[10px]">
+                      <Image src={ethereum} width={13} height={17} alt='ethereum' />
+                      <p className="text-xs-bold font-NunitoSansSemiBold text-slate-400">{tc.ethereum}</p>
+                    </div>
+                    <p className="text-xl-medium text-orange-500 font-NunitoSansBold ml-6">{tc.changes}</p>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      </section>
+      <section className="py-10">
+        <div className="mx-auto max-w-xl w-full">
+          <h3 className="font-NunitoSansBold text-2lg-bold text-center text-slate-900 mb-[72px]">Create and sell your MERET NFT</h3>
+          <ul className="flex items-center justify-between">
+            <li className="max-w-[296px] w-full">
+              <h5 className="text-center text-xl-medium text-slate-600 font-NunitoSansSemiBold w-full mb-6">Resources for getting started</h5>
+              <p className="text-center text-md-semibold text-slate-600 font-NunitoSansRegular w-full">Once you’ve set up your wallet of choice, connect it to OpenSea by clicking the wallet icon in the top right corner. Learn about the wallets we support.</p>
+            </li>
+            <li className="max-w-[264px] w-full">
+              <h5 className="text-center text-xl-medium text-slate-600 font-NunitoSansSemiBold w-full mb-6">Create your collection</h5>
+              <p className="text-center text-md-semibold text-slate-600 font-NunitoSansRegular w-full">Click My Collections and set up your collection. Add social links, a description, profile & banner images, and set a secondary sales fee.</p>
+            </li>
+            <li className="max-w-[264px] w-full">
+              <h5 className="text-center text-xl-medium text-slate-600 font-NunitoSansSemiBold w-full mb-6">Add your MERET NFT</h5>
+              <p className="text-center text-md-semibold text-slate-600 font-NunitoSansRegular w-full">Upload your work (image, video, audio, or 3D art), add a title and description, and customize your NFTs with properties, stats, and unlockable content.</p>
+            </li>
+            <li className="max-w-[264px] w-full">
+              <h5 className="text-center text-xl-medium text-slate-600 font-NunitoSansSemiBold w-full mb-6">List them for sale</h5>
+              <p className="text-center text-md-semibold text-slate-600 font-NunitoSansRegular w-full">Choose between auctions, fixed-price listings, and declining-price listings. You choose how you want to sell your NFTs, and we help you sell them!</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className="py-10">
+        <div className="mx-auto max-w-xl w-full">
+          <h3 className="text-2lg-bold font-NunitoSansBold text-slate-900 text-center mb-[60px]">Resources for getting started</h3>
+          <ul className="flex items-center justify-between w-full">
+            {
+              resources.map((r) => {
+                return (
+                  <li>
+                    <Image src={r.image} width={391} height={199} alt='res img' />
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      </section>
+      <section className="py-16">
+        <div className="mx-auto max-w-xl w-full">
+          <h2 className="mb-4 text-center font-NunitoSansSemiBold text-xl-bold text-slate-900">Meet MERET NFT Market Place</h2>
+          <p className="mb-9 text-center font-NunitoSansRegular text-base-regular text-slate-600">The Meet MERET NFT Market Place with everything for everyone</p>
+          <div className="w-full relative cursor-pointer">
+            <Image src={heroVideo} width={1200} height={693} alt='heroVideo' />
+            <Image src={VideoStarter} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" width={131} height={131} alt='VideoStarter' />
           </div>
         </div>
       </section>
